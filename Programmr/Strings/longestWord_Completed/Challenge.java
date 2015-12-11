9fb77b2d7b20bb516d21614905e9e38edefb30c8
BaleaCristi <balea.cristian90@gmail.com>
2015-12-11 14:43:56 +0200

    small edit longest_word

--- Programmr/Strings/longestWord_Completed/Challenge.java
+++ Programmr/Strings/longestWord_Completed/Challenge.java
@@ -11,22 +11,16 @@ public class Challenge {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String s1 = br.readLine();
 		String longestWord = "";
+		/// {Enter your code here
 		String[] words = s1.split(" ");
 
 		for (int i = 0; i < words.length; i++) {
-
-			System.out.println("first: " + longestWord);
-
 			if (longestWord.length() > words[i].length()) {
 				longestWord = longestWord;
 			} else {
 				longestWord = words[i];
 			}
-			System.out.println("second: " + longestWord);
 		}
-
-		/// {Enter your code here
-
 		/// }
 		System.out.println("The longest word in the sentence is:" + longestWord);
 	}
