22c135aa67fea45e4003ded8af184ec165134a95
BaleaCristi <balea.cristian90@gmail.com>
2015-12-21 10:53:08 +0200

    Added some Methods

---
+++ Programmr/Methods/convertUpperToLowerCase/Challenge.java
@@ -0,0 +1,23 @@
+package convertUpperToLowerCase;
+
+import java.util.Scanner;
+
+class Challenge {
+
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter an alphabet:");
+		String input;
+
+		input = scanner.next();
+		input = lowerCase(input);
+		System.out.println(input);
+	}
+
+	public static String lowerCase(String x) {
+		/// {Write your code here
+		return null;
+		/// }
+	}
+
+}
---
+++ Programmr/Methods/squareRootOfANumber/Challenge.java
@@ -0,0 +1,25 @@
+package squareRootOfANumber;
+
+import java.util.Scanner;
+
+public class Challenge {
+
+	public static double number, answer;
+
+	public static void calculateSquare(double number) {
+		/// Write your code here
+
+		///
+	}
+
+	public static void main(String[] args) {
+
+		Challenge challenge = new Challenge();
+		final Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		number = sc.nextDouble();
+		challenge.calculateSquare(number);
+
+	}
+
+}
