390a2023103707222592d8a8288a8170796a4e54
BaleaCristi <balea.cristian90@gmail.com>
2015-12-21 16:45:01 +0200

    Added few loops

---
+++ Programmr/Loops/countingTheNumberOfA/Challenge.java
@@ -0,0 +1,22 @@
+package countingTheNumberOfA;
+
+import java.util.Scanner;
+
+class Challenge {
+
+	public static void main(String args[]) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter String: ");
+		String str = sc.nextLine();
+
+		/// { Write your code here
+
+		/// }
+
+		// System.out.println("Number of a's: " + count);
+
+	}
+
+}
---
+++ Programmr/Loops/sumOfPrimeDigitsInANumber/Challenge.java
@@ -0,0 +1,22 @@
+package sumOfPrimeDigitsInANumber;
+
+import java.util.Scanner;
+
+class Challenge {
+
+	public static void main(String args[]) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter String: ");
+		String str = sc.nextLine();
+		// Start
+		/// { Write your code here
+
+		/// }
+
+		// System.out.println("Number of a's: " + count);
+		// End
+	}
+
+}
