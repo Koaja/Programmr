1fd874189021a14283698be524f5ab52d6254046
BaleaCristi <balea.cristian90@gmail.com>
2015-12-21 16:11:24 +0200

    added collections

---
+++ Programmr/Collections/stack/Challenge.java
@@ -0,0 +1,25 @@
+package stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+class Challenge {
+
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		int sum = 0;
+
+		System.out.println("Enter the 1st number:");
+		int num1 = scanner.nextInt();
+		System.out.println("Enter the 2nd number:");
+		int num2 = scanner.nextInt();
+		System.out.println("Enter the 3rd number:");
+		int num3 = scanner.nextInt();
+		Stack<Integer> st = new Stack<Integer>();
+		/// {write you code here
+
+		/// }
+
+	}
+
+}
