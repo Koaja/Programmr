16bc2a05973a6d1ec8af29ea191bf30a708bc336
BaleaCristi <balea.cristian90@gmail.com>
2015-12-22 15:36:46 +0200

    added some more loop exercises

---
+++ Programmr/Loops/sumOfFiveIntegers/Challenge.java
@@ -0,0 +1,15 @@
+package sumOfFiveIntegers;
+
+import java.util.Scanner;
+
+public class Challenge {
+	public static void main(String[] args) {
+		int total = 0; // stores the sum of the 5 numbers
+		int number; // stores the current input
+		Scanner scan = new Scanner(System.in);
+		/// { Write your code here
+
+		/// }
+		System.out.println("Total is:" + total);
+	}
+}
